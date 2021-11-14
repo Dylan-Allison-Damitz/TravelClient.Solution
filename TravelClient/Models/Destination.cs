689c5b47e79b96723b23f2ce789e8a7fd820d2b1
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-11-14 15:35:09 -0800

    add all changes 11/14 @ 3:35pm

---
+++ TravelClient/Models/Destination.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TravelClient.Models
+{
+  public class Destination
+  {
+    public int DestinationId { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Description { get; set; }
+
+    public int Rating { get; set; }
+
+    public string Review { get; set; }
+
+    public static List<Destination> GetDestinations()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Destination> destinationList = JsonConvert.DeserializeObject<List<Destination>>(jsonResponse.ToString());
+
+      return destinationList;
+    }
+
+    public class Pagination
+    {
+      public int PageNumber { get; set; }
+      public int PageSize { get; set; }
+      public int? TotalRecords { get; set; }
+      public int? TotalPages => TotalRecords.HasValue ? (int)Math.Ceiling(TotalRecords.Value / (double)PageSize) : (int?)null;
+    }
+
+    public static Destination GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Destination destination = JsonConvert.DeserializeObject<Destination>(jsonResponse.ToString());
+
+      return destination;
+    }
+    public static void Post(Destination destination)
+    {
+      string jsonDestination = JsonConvert.SerializeObject(destination);
+      var apiCallTask = ApiHelper.Post(jsonDestination);
+    }
+
+    public static void Put(Destination destination)
+    {
+      string jsonDestination = JsonConvert.SerializeObject(destination);
+      var apiCallTask = ApiHelper.Put(destination.DestinationId, jsonDestination);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete(id);
+    }
+  }
+}
